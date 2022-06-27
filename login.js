adfc956d481e418b4bf045e595d7642e743f2f59
your name <sagnik2kz@gmail.com>
2022-06-27 13:55:16 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,45 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+
+const app = express();
+app.use("/assets",express.static("assets"));
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "letmein",
+    database: "login"
+});
+
+connection.connect(function(error){
+    if (error) throw error
+    else console.log("connected to the database successfully!")
+});
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",encoder, function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("SELECT * FROM login.students where student_id = ? and password = ?",[student_id,password],function(error,results,fields){
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
+})
+
+
+app.listen(3000);
